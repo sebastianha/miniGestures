f6ae6740ef417ad92fe899fe27478e940c793344
Sebastian Hammerl <github@omoco.de>
2016-01-24 17:48:08 +0100

    Cleaned up code

--- mouseTrack.js
+++ mouseTrack.js
@@ -1,352 +1,284 @@
-/*   
+/*
  *  Copyright (C) 2013  AJ Ribeiro
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.   
-*/
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+"use strict";
 
-var rmousedown=false, moved=false, lmousedown=false
-var rocker=false, trail=false
-var mx,my,nx,ny,lx,ly,phi
-var move="", omove=""
-var pi =3.14159
-var suppress=1
-var canvas, myGests, ginv
-var link, ls, myColor="red", myWidth=3
-var loaded=false
-var rocked=false
-var link=null
+var rmousedown = false, moved = false, lmousedown = false;
+var rocker = false, trail = false;
+var mx, my, nx, ny, lx, ly, phi;
+var move = "", omove = "";
+var pi = 3.14159;
+var suppress = 1;
+var canvas, myGests, ginv;
+var myColor = "red", myWidth = 3;
+var loaded = false;
+var rocked = false;
+var link = null;
 
-function invertHash(hash)
-{
-    inv = {}
-    for(key in hash)
-        inv[hash[key]] = key
-    return inv
+function invertHash(hash) {
+	var inv = {};
+	for(var key in hash) {
+		if(hash.hasOwnProperty(key)) {
+			inv[hash[key]] = key;
+		}
+	}
+	return inv;
 }
 
-function createCanvas()
-{
-    canvas = document.createElement('canvas');
-    canvas.id = "gestCanvas"
-    canvas.style.width=document.body.scrollWidth
-    canvas.style.height=document.body.scrollHeight
-    canvas.width=window.document.body.scrollWidth
-    canvas.height=window.document.body.scrollHeight     
-    canvas.style.left="0px";
-    canvas.style.top="0px";
-    canvas.style.overflow = 'visible';
-    canvas.style.position = 'absolute';
-    canvas.style.zIndex="10000"
+function createCanvas() {
+	canvas = document.createElement("canvas");
+	canvas.id = "gestCanvas";
+	canvas.style.width = document.body.scrollWidth;
+	canvas.style.height = document.body.scrollHeight;
+	canvas.width = window.document.body.scrollWidth;
+	canvas.height = window.document.body.scrollHeight;
+	canvas.style.left = "0px";
+	canvas.style.top = "0px";
+	canvas.style.overflow = "visible";
+	canvas.style.position = "absolute";
+	canvas.style.zIndex = "10000";
 }
-function draw(x,y){
-    var ctx = document.getElementById('gestCanvas').getContext('2d');
-    ctx.beginPath();
-    ctx.strokeStyle = myColor
-    ctx.lineWidth = myWidth
-    ctx.moveTo(lx,ly);
-    ctx.lineTo(x,y);
-    ctx.stroke()
-    lx=x
-    ly=y
+function draw(x, y) {
+	var ctx = document.getElementById("gestCanvas").getContext("2d");
+	ctx.beginPath();
+	ctx.strokeStyle = myColor;
+	ctx.lineWidth = myWidth;
+	ctx.moveTo(lx, ly);
+	ctx.lineTo(x, y);
+	ctx.stroke();
+	lx = x;
+	ly = y;
 }
 
-document.onmousedown = function(event){
-    if(event.which == 1){
-        lmousedown = true
-    }
-    else if(event.which == 3){
-        rmousedown = true
-    }
+document.onmousedown = function(event) {
+	if(event.which === 1) {
+		lmousedown = true;
+	} else if(event.which === 3) {
+		rmousedown = true;
+	}
 
-    //leftrock
-    if(event.which == 1 && rmousedown && suppress && rocker){
-        if(! loaded){
-            loadOptions()
-            loaded=true
-        }
-        move = 'back'
-        rocked = true
-        // console.log(rocked)
-        exeRock()
-    }
+	//leftrock
+	if(event.which === 1 && rmousedown && suppress && rocker) {
+		if(!loaded) {
+			loadOptions();
+			loaded = true;
+		}
+		move = "back";
+		rocked = true;
+		exeRock();
+	}
 
-    // console.log('rmousedown '+suppress)
-    //right mouse click
-    else if(event.which == 3 && suppress){
-        if(! loaded){
-            loadOptions()
-            loaded=true
-        }
-        if(lmousedown && rocker){
-            if(! loaded){
-                loadOptions()
-                loaded=true
-            }
-            move = 'forward'
-            rocked = true
-            // console.log(rocked)
-            exeRock()
-        }
-        else{
-            my = event.pageX;
-            mx = event.pageY;
-            lx = my
-            ly = mx
-            move = ""
-            omove=""
-            moved=false
-            if(event.target.href){
-                link = event.target.href
-            }
-            else if(event.target.parentElement.href){
-                link = event.target.parentElement.href
-            }
-            else{
-                link = null
-            }
-        }
-    }
+	//right mouse click
+	else if(event.which === 3 && suppress) {
+		if(!loaded) {
+			loadOptions();
+			loaded = true;
+		}
+		if(lmousedown && rocker) {
+			if(!loaded) {
+				loadOptions();
+				loaded = true;
+			}
+			move = "forward";
+			rocked = true;
+			exeRock();
+		} else {
+			my = event.pageX;
+			mx = event.pageY;
+			lx = my;
+			ly = mx;
+			move = "";
+			omove = "";
+			moved = false;
+			if(event.target.href) {
+				link = event.target.href;
+			} else if(event.target.parentElement.href) {
+				link = event.target.parentElement.href;
+			} else {
+				link = null;
+			}
+		}
+	}
 
 };
 
-document.onmousemove = function(event)
-{
-    //track the mouse if we are holding the right button
-    if(rmousedown)
-    {
-        ny = event.pageX;
-        nx = event.pageY;
-        var r = Math.sqrt(Math.pow(nx-mx,2)+Math.pow(ny-my,2))
-        if(r > 16)
-        {
-            phi = Math.atan2(ny-my,nx-mx)
-            if(phi < 0) phi += 2.*pi
-            if(phi >= pi/4. && phi < 3.*pi/4.)
-                var tmove="R"
-            else if(phi >= 3.*pi/4. && phi < 5.*pi/4.)
-                var tmove="U"
-            else if(phi >= 5.*pi/4. && phi < 7.*pi/4.)
-                var tmove="L"
-            else if(phi >= 7.*pi/4. || phi < pi/4.)
-                var tmove="D"
-            if(tmove != omove)
-            {
-                move += tmove
-                omove = tmove
-            }
-            if(moved == false)
-            {
-                // console.log('making canvas')
-                createCanvas()
-                document.body.appendChild(canvas);
-            }
-            moved=true
-            // console.log('indraw'+trail)
+document.onmousemove = function(event) {
+	//track the mouse if we are holding the right button
+	if(rmousedown) {
+		ny = event.pageX;
+		nx = event.pageY;
+		var r = Math.sqrt(Math.pow(nx - mx, 2) + Math.pow(ny - my, 2));
+		if(r > 16) {
+			phi = Math.atan2(ny - my, nx - mx);
+			if(phi < 0) {
+				phi += 2.0 * pi;
+			}
+			var tmove;
+			if(phi >= pi / 4.0 && phi < 3.0 * pi / 4.0) {
+				tmove = "R";
+			} else if(phi >= 3.0 * pi / 4. && phi < 5.0 * pi / 4.0) {
+				tmove = "U";
+			} else if(phi >= 5.0 * pi / 4.0 && phi < 7.0 * pi / 4.0) {
+				tmove = "L";
+			} else if(phi >= 7.0 * pi / 4.0 || phi < pi / 4.0) {
+				tmove = "D";
+			}
+			if(tmove !== omove) {
+				move += tmove;
+				omove = tmove;
+			}
+			if(moved === false) {
+				createCanvas();
+				document.body.appendChild(canvas);
+			}
+			moved = true;
 
-            if(trail){
-                // console.log('draw')
-                draw(ny,nx)
-            }
+			if(trail) {
+				draw(ny, nx);
+			}
 
-            mx = nx
-            my = ny
-        }
-    }
+			mx = nx;
+			my = ny;
+		}
+	}
 };
 
 
-document.onmouseup = function(event)
-{
-    // console.log('mouse is up '+suppress)
-    if(event.which == 1)
-        lmousedown = false
+document.onmouseup = function(event) {
+	if(event.which === 1) {
+		lmousedown = false;
+	}
 
-    //right mouse release
-    if(event.which == 3){
-        // console.log('suppress is '+suppress)
-        rmousedown=false
-        if(moved){
-            cvs = document.getElementById('gestCanvas')
-            if(cvs)
-            {
-                // document.body.removeChild(link)
-                document.body.removeChild(canvas)
-                cvs.width = cvs.width;
-            }
-            exeFunc()
-        }
-        else if(rocked){
-            rocked = false
-        }
-        else{
-            --suppress
-            // console.log('no move '+suppress)
-            // $('#target').rmousedown(which=3);
-        }
-    }
+	//right mouse release
+	if(event.which === 3) {
+		rmousedown = false;
+		if(moved) {
+			var cvs = document.getElementById("gestCanvas");
+			if(cvs) {
+				// document.body.removeChild(link)
+				document.body.removeChild(canvas);
+				cvs.width = cvs.width;
+			}
+			exeFunc();
+		} else if(rocked) {
+			rocked = false;
+		} else {
+			--suppress;
+			// $("#target").rmousedown(which=3);
+		}
+	}
 };
-function exeRock()
-{
-    action = move
-    if(action == "back")
-    {
-        window.history.back()
-    }
-    else if(action == "forward")
-    {
-        window.history.forward()
-    }
+function exeRock() {
+	var action = move;
+	if(action === "back") {
+		window.history.back();
+	} else if(action === "forward") {
+		window.history.forward();
+	}
 }
 
-function exeFunc()
-{
-    // console.log('exeFunc '+move)
-    if(ginv[move])
-    {
-        action = ginv[move]
-        if(action == "back")
-        {
-            window.history.back()
-        }
-        else if(action == "forward")
-        {
-            window.history.forward()
-        }
-        else if(action == "newtab")
-        {
-            if(link == null){
-                chrome.extension.sendMessage({msg: "newtab"}, 
-                    function(response)
-                    {
-                        if(response != null)
-						{
-                            // console.log(response.resp);
-						}
-                        else
-                        {
-                            //console.log('problem executing open tab')
-                            if(chrome.extension.lastError)
-							{
-                                // console.log(chrome.extension.lastError.message)
-							}
-                        }
-                    });
-            }
-            else{
-                window.open(link)
-            }
-        }
-        else if(action == "closetab"){
-            chrome.extension.sendMessage({msg: "closetab"});
-        }
-        else if(action == "lasttab"){
-            chrome.extension.sendMessage({msg: "lasttab"});
-        }
-        else if(action == "reloadall"){
-            chrome.extension.sendMessage({msg: "reloadall"});
-        }
-
-        else if(action == "closeall"){
-            chrome.extension.sendMessage({msg: "closeall"});
-        }
-
-        else if(action == "nexttab") {
-            chrome.extension.sendMessage({msg: "nexttab"});
-        }
-
-        else if(action == "prevtab"){
-            chrome.extension.sendMessage({msg: "prevtab"});
-        }
-
-        else if(action == "closeback"){
-            chrome.extension.sendMessage({msg: "closeback"});
-        }
-
-
-        else if(action == "scrolltop") 
-            window.scrollTo(0,0)
-
-        else if(action == "scrollbottom") 
-            window.scrollTo(0,document.body.scrollHeight)
-
-        else if(action == "reload")
-            window.location.reload()
-
-        else if(action == "stop")
-            window.stop()
-
-    }
+function exeFunc() {
+	if(ginv[move]) {
+		var action = ginv[move];
+		if(action === "back") {
+			window.history.back();
+		} else if(action === "forward") {
+			window.history.forward();
+		} else if(action === "newtab") {
+			if(link === null) {
+				chrome.extension.sendMessage({msg: "newtab"}, function(response) {
+				});
+			} else {
+				window.open(link);
+			}
+		} else if(action === "closetab") {
+			chrome.extension.sendMessage({msg: "closetab"});
+		} else if(action === "lasttab") {
+			chrome.extension.sendMessage({msg: "lasttab"});
+		} else if(action === "reloadall") {
+			chrome.extension.sendMessage({msg: "reloadall"});
+		} else if(action === "closeall") {
+			chrome.extension.sendMessage({msg: "closeall"});
+		} else if(action === "nexttab") {
+			chrome.extension.sendMessage({msg: "nexttab"});
+		} else if(action === "prevtab") {
+			chrome.extension.sendMessage({msg: "prevtab"});
+		} else if(action === "closeback") {
+			chrome.extension.sendMessage({msg: "closeback"});
+		} else if(action === "scrolltop") {
+			window.scrollTo(0, 0);
+		} else if(action === "scrollbottom") {
+			window.scrollTo(0, document.body.scrollHeight);
+		} else if(action === "reload") {
+			window.location.reload();
+		} else if(action === "stop") {
+			window.stop();
+		}
+	}
 }
 
 
-document.oncontextmenu = function()
-{
-    // console.log('ctx menu suppress is '+suppress)
-    if(suppress)
-        return false
-    else{
-        // console.log("open it");
-        suppress++
-        return true
-    }
+document.oncontextmenu = function() {
+	if(suppress) {
+		return false;
+	} else {
+		suppress++;
+		return true;
+	}
 };
 
-function loadOptions(name)
-{
-    chrome.extension.sendMessage({msg: "colorCode"}, 
-        function(response) {
-            if(response){
-                // console.log('color'+response.resp)
-                myColor = response.resp
-            }
-            // else
-            //     console.log('error getting colorCode')
-        });
-    chrome.extension.sendMessage({msg: "width"}, 
-        function(response) {
-            if(response){
-                myWidth = response.resp
-                // console.log('width '+myWidth)
-            }
-            // else
-            //     console.log('error getting width')
-        });
-    chrome.extension.sendMessage({msg: "gests"}, 
-        function(response) 
-        {
-            if(response)
-                myGests = response.resp
-            ginv = invertHash(myGests)
-        });
+function loadOptions(name) {
+	chrome.extension.sendMessage({msg: "colorCode"}, function(response) {
+		if(response) {
+			myColor = response.resp;
+		}
+	});
+	chrome.extension.sendMessage({msg: "width"}, function(response) {
+		if(response) {
+			myWidth = response.resp;
+		}
+	});
+	chrome.extension.sendMessage({msg: "gests"}, function(response) {
+		if(response) {
+			myGests = response.resp;
+		}
+		ginv = invertHash(myGests);
+	});
 
-    chrome.extension.sendMessage({msg: "rocker"}, 
-        function(response) 
-        {
-            if(response)
-                rocker = response.resp
-            if(rocker == 'true') rocker = true
-            else rocker = false
-        });
+	chrome.extension.sendMessage({msg: "rocker"}, function(response) {
+		if(response) {
+			rocker = response.resp;
+		}
+		if(rocker == "true") {
+			rocker = true;
+		} else {
+			rocker = false;
+		}
+	});
 
-    chrome.extension.sendMessage({msg: "trail"}, 
-        function(response) 
-        {
-            if(response)
-                trail = response.resp
-            if(trail == 'true') trail = true
-            else trail = false
-        });
+	chrome.extension.sendMessage({msg: "trail"}, function(response) {
+		if(response) {
+			trail = response.resp;
+		}
+		if(trail == "true") {
+			trail = true;
+		} else {
+			trail = false;
+		}
+	});
 }
 
-document.addEventListener('DOMContentLoaded', loadOptions);
+document.addEventListener("DOMContentLoaded", loadOptions);
--- options.js
+++ options.js
@@ -1,163 +1,182 @@
-/*   
+/*
  *  Copyright (C) 2013  AJ Ribeiro
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.   
-*/
-colorCodes={"red":"ff3300","green":"008000","blue":"00008B",
-                            "yellow":"FFFF00"}
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+"use strict";
 
-colorNames={"ff3300":"red","008000":"green","00008B":"blue",
-                            "FFFF00":"yellow"}
+var colorCodes = {
+	"red"   : "ff3300",
+	"green" : "008000",
+	"blue"  : "00008B",
+	"yellow": "FFFF00"
+};
 
-defaultGests={"U":"newtab","R":"forward","L":"back","UD":"closetab"}
+var colorNames = {
+	"ff3300": "red",
+	"008000": "green",
+	"00008B": "blue",
+	"FFFF00": "yellow"
+};
 
-commandTrans={"History Back":"back","History Forward":"forward",
-                            "Reload":"reload","Stop Loading":"stop",
-                            "Open New Tab":"newtab","Close Current Tab":"closetab",
-                            "Close Background Tabs":"closeback","Close Window":"closeall",
-                            "Reload All Tabs":"reloadall",
-                            "Next Tab":"nexttab","Previous Tab":"prevtab",
-                            "Scroll to Top":"scrolltop", "Scroll to Bottom":"scrollbottom",
-                            "Re-open Last Closed Tab":"lasttab",
-                            }
+var defaultGests = {
+	"U" : "newtab",
+	"R" : "forward",
+	"L" : "back",
+	"UD": "closetab"
+};
 
+var commandTrans = {
+	"History Back"           : "back",
+	"History Forward"        : "forward",
+	"Reload"                 : "reload",
+	"Stop Loading"           : "stop",
+	"Open New Tab"           : "newtab",
+	"Close Current Tab"      : "closetab",
+	"Close Background Tabs"  : "closeback",
+	"Close Window"           : "closeall",
+	"Reload All Tabs"        : "reloadall",
+	"Next Tab"               : "nexttab",
+	"Previous Tab"           : "prevtab",
+	"Scroll to Top"          : "scrolltop",
+	"Scroll to Bottom"       : "scrollbottom",
+	"Re-open Last Closed Tab": "lasttab"
+};
 
-function invertHash(hash)
-{
-    inv = {}
-    for(key in hash)
-        inv[hash[key]] = key
-    return inv
+function invertHash(hash) {
+	var inv = {};
+	for(var key in hash) {
+		if(hash.hasOwnProperty(key)) {
+			inv[hash[key]] = key;
+		}
+	}
+	return inv;
 }
 
-function fillMenu()
-{
-    var key,div,tr,td,select,inp,img,a
-    gests = {}
-    for(key in localStorage)
-    {
-        if(key == "colorCode" || key == "width")
-            continue
-        gests[key]=localStorage[key]
-    }
-    if(Object.keys(gests).length == 0)
-        gests = invertHash(defaultGests)
-    // availG = invertHash(gests)
-    div = document.getElementById("optsTab");
-    for(key in commandTrans)
-    {
-        tr=div.insertRow(div.rows.length)
-        td=document.createElement('td')
-        td.appendChild(document.createTextNode(key))
-        tr.appendChild(td)
-        td=document.createElement('td')
-        inp=document.createElement('input')
-        inp.type='text'
-        if(gests[commandTrans[key]])
-            inp.value=gests[commandTrans[key]]
-        td.align='center'
-        tr.appendChild(td)
-        td.appendChild(inp)
-    }
-}
+function fillMenu() {
+	var key, div, tr, td, inp;
+	var gests = {};
 
+	for(key in localStorage) {
+		if(localStorage.hasOwnProperty(key)) {
+			if(key === "colorCode" || key === "width") {
+				continue;
+			}
+			gests[key] = localStorage[key];
+		}
+	}
+
+	if(Object.keys(gests).length === 0) {
+		gests = invertHash(defaultGests);
+	}
+
+	// availG = invertHash(gests)
+	div = document.getElementById("optsTab");
+	for(key in commandTrans) {
+		if(commandTrans.hasOwnProperty(key)) {
+			tr = div.insertRow(div.rows.length);
+			td = document.createElement("td");
+			td.appendChild(document.createTextNode(key));
+			tr.appendChild(td);
+			td = document.createElement("td");
+			inp = document.createElement("input");
+			inp.type = "text";
+			if(gests[commandTrans[key]]) {
+				inp.value = gests[commandTrans[key]];
+			}
+			td.align = "center";
+			tr.appendChild(td);
+			td.appendChild(inp);
+		}
+	}
+}
 
 // Saves options to localStorage.
-function save_options() 
-{
-    var select, value
-    
-    select = document.getElementById("color");
-    value = select.children[select.selectedIndex].value;
-    localStorage["colorCode"]=colorCodes[value];
+function saveOptions() {
+	var select = document.getElementById("color");
+	var value = select.children[select.selectedIndex].value;
+	localStorage.colorCode = colorCodes[value];
 
-    select = document.getElementById("width");
-    localStorage["width"]=select.children[select.selectedIndex].value;
+	select = document.getElementById("width");
+	localStorage.width = select.children[select.selectedIndex].value;
 
-    var rocker = document.getElementById('rocker')
-    if(rocker.checked){
-        localStorage['rocker'] = true
-    }
-    else{
-        localStorage['rocker'] = false
-    }
+	var rocker = document.getElementById("rocker");
+	if(rocker.checked) {
+		localStorage.rocker = true;
+	} else {
+		localStorage.rocker = false;
+	}
 
-    var trail = document.getElementById('trail')
-    if(trail.checked){
-        localStorage['trail'] = true
-    }
-    else{
-        localStorage['trail'] = false
-    }
+	var trail = document.getElementById("trail");
+	if(trail.checked) {
+		localStorage.trail = true;
+	} else {
+		localStorage.trail = false;
+	}
 
-    // Update status to let user know options were saved.
-    var status = document.getElementById("status");
-    status.innerHTML = "Configuration Saved";
-    setTimeout(
-        function()
-        {
-            status.innerHTML = "";
-        }, 750);
+	// Update status to let user know options were saved.
+	var status = document.getElementById("status");
+	status.innerHTML = "Configuration Saved";
+	setTimeout(function() {
+		status.innerHTML = "";
+	}, 750);
 
-    inputs = document.getElementsByTagName('input')
-    for(i=0;i<inputs.length;i++)
-    {
-        s = inputs[i].parentElement.parentElement.children[0].textContent
-        if(inputs[i].value.length > 0)
-            localStorage.setItem(commandTrans[s],inputs[i].value)
-        else
-            localStorage.removeItem(commandTrans[s])
-    }
+	var inputs = document.getElementsByTagName("input");
+	for(var i = 0; i < inputs.length; i++) {
+		var s = inputs[i].parentElement.parentElement.children[0].textContent;
+		if(inputs[i].value.length > 0) {
+			localStorage.setItem(commandTrans[s], inputs[i].value);
+		} else {
+			localStorage.removeItem(commandTrans[s]);
+		}
+	}
 
 }
 
 // Restores select box state to saved value from localStorage.
-function restore_options() 
-{
-    var select, value
-    select = document.getElementById("color");
-    value = colorNames[localStorage["colorCode"]]
-    if(!value) value = "red"
-    for (var i = 0; i < select.children.length; i++) 
-    {
-        var child = select.children[i];
-        if (child.value == value)
-        {
-            child.selected = "true";
-            break;
-        }
-    }
+function restoreOptions() {
+	var select = document.getElementById("color");
+	var value = colorNames[localStorage.colorCode];
+	if(!value) {
+		value = "red";
+	}
+	for(var i = 0; i < select.children.length; i++) {
+		var child = select.children[i];
+		if(child.value === value) {
+			child.selected = "true";
+			break;
+		}
+	}
 
-    select = document.getElementById("width");
-    value = localStorage["width"]
-    if(! value) value = 3
-    for (var i = 0; i < select.children.length; i++) 
-    {
-        var child = select.children[i];
-        if (child.value == value) 
-        {
-            child.selected = "true";
-            break;
-        }
-    }
+	select = document.getElementById("width");
+	value = localStorage.width;
+	if(!value) {
+		value = 3;
+	}
+	for(var ii = 0; ii < select.children.length; ii++) {
+		var child2 = select.children[ii];
+		if(child2.value === value) {
+			child2.selected = "true";
+			break;
+		}
+	}
 }
 
-function loadInfo()
-{
-    restore_options()
-    fillMenu()
+function loadInfo() {
+	restoreOptions();
+	fillMenu();
 }
 
-document.addEventListener('DOMContentLoaded', loadInfo);
-document.querySelector('#save').addEventListener('click', save_options);
+document.addEventListener("DOMContentLoaded", loadInfo);
+document.querySelector("#save").addEventListener("click", saveOptions);
